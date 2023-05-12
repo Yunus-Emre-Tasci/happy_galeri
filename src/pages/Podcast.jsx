5b3923c67f9bd4f76fd4ab5ee266cfd023cad8cb
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-05-12 11:52:15 +0300

    podcast

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import european1 from "../assests/european1.png"
 
 const Podcast = () => {
-  return <div className="h-[2129px] bg-yellow-500">Podcast</div>;
+  return (
+    <div className="h-[2129px] bg-yellow-500  ">
+      <div className="bg-gradient-to-br from-gray-300 to-gray-200 w-[1860px] h-[1327px] mt-[161px] mx-auto rounded-xl">
+        <div>
+          <div>
+            <div>
+              <h3>Podcast</h3>
+              <p>
+                bu muhteşem müzede keşfedilecek o kadar çok sanat eseri var ki!
+                Bu yüzden en iyi şekilde .
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna.
+              </p>
+              <p>SPOTİFAY</p>
+            </div>
+            <div className="flex">
+              <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+              </div>
+              <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+              </div>
+              <div>
+                <div>
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <img src="" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="relative w-[386px] h-[486px] text-white">
+              <img className="w-full h-full" src={european1} alt="" />
+              <div className="absolute top-2 left-1">
+                <h4>EUROPEAN</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <h4>EUROPEAN</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <h4>EUROPEAN</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </p>
+              </div>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <h4>EUROPEAN</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Podcast
