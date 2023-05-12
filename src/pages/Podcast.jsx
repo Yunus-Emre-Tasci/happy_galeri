43fe2541d186ffee47e30edf912b5cdcfd196f06
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-05-12 14:01:20 +0300

    podcast-buttom

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -2,93 +2,103 @@ import european1 from "../assests/european1.png"
 
 const Podcast = () => {
   return (
-    <div className="h-[2129px] bg-yellow-500  ">
-      <div className="bg-gradient-to-br from-gray-300 to-gray-200 w-[1860px] h-[1327px] mt-[161px] mx-auto rounded-xl">
-        <div>
+    <div className="h-[2129px] bg-yellow-500 relative ">
+      
+        <div className="bg-gradient-to-br from-gray-300 to-gray-200 w-[1860px] h-[1327px] absolute left-[30px] top-[161px] rounded-xl">
           <div>
             <div>
-              <h3>Podcast</h3>
-              <p>
-                bu muhteşem müzede keşfedilecek o kadar çok sanat eseri var ki!
-                Bu yüzden en iyi şekilde .
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna.
-              </p>
-              <p>SPOTİFAY</p>
+              <div>
+                <h3>Podcast</h3>
+                <p>
+                  bu muhteşem müzede keşfedilecek o kadar çok sanat eseri var
+                  ki! Bu yüzden en iyi şekilde .
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna.
+                </p>
+                <p>SPOTİFAY</p>
+              </div>
+              <div className="flex">
+                <div>
+                  <img src="" alt="" />
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <img src="" alt="" />
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <div>
+                    <img src="" alt="" />
+                  </div>
+                  <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                  </div>
+                  <div>
+                    <img src="" alt="" />
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="flex">
-              <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
-              </div>
-              <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
-              </div>
-              <div>
-                <div>
-                  <img src="" alt="" />
-                </div>
-                <div>
-                  <img src="" alt="" />
-                  <img src="" alt="" />
-                </div>
-                <div>
-                  <img src="" alt="" />
+              <div className="relative w-[386px] h-[486px] text-white">
+                <img className="w-full h-full" src={european1} alt="" />
+                <div className="absolute top-2 left-1">
+                  <h4>EUROPEAN</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna.
+                  </p>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="relative w-[386px] h-[486px] text-white">
-              <img className="w-full h-full" src={european1} alt="" />
-              <div className="absolute top-2 left-1">
-                <h4>EUROPEAN</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna.
-                </p>
-              </div>
-            </div>
-            <div>
-              <img src="" alt="" />
               <div>
-                <h4>EUROPEAN</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna.
-                </p>
+                <img src="" alt="" />
+                <div>
+                  <h4>EUROPEAN</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div>
-              <img src="" alt="" />
               <div>
-                <h4>EUROPEAN</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna.
-                </p>
+                <img src="" alt="" />
+                <div>
+                  <h4>EUROPEAN</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div>
-              <img src="" alt="" />
               <div>
-                <h4>EUROPEAN</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna.
-                </p>
+                <img src="" alt="" />
+                <div>
+                  <h4>EUROPEAN</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="flex">
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto accusantium neque architecto unde nihil illo, similique odit, labore quidem libero, voluptatem incidunt quaerat perspiciatis id quia distinctio facilis officia reprehenderit.</div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
       </div>
-      <div>
-        
-      </div>
-    </div>
+    
   );
 }
 
